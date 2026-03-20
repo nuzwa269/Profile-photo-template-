959c957e82903c40faff6a8cb1ad9fe8c73f2892
nuzwa269 <blsoha424@gmail.com>
2026-03-20 20:06:46 +0500

    Add asset URL and path helper functions

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,1 +1,13 @@
+<?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+function ppt_get_asset_url( $relative_path ) {
+	return PPT_URL . ltrim( $relative_path, '/' );
+}
+
+function ppt_get_asset_path( $relative_path ) {
+	return PPT_PATH . ltrim( $relative_path, '/' );
+}
