d05418f5628870b8f37754fb3423b84750f56887
nuzwa269 <blsoha424@gmail.com>
2026-03-20 20:08:48 +0500

    Update class-ppt-shortcode.php

--- includes/class-ppt-shortcode.php
+++ includes/class-ppt-shortcode.php
@@ -1,1 +1,21 @@
+<?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class PPT_Shortcode {
+
+	public function init() {
+		add_shortcode( 'ai_profile_tool', array( $this, 'render_shortcode' ) );
+	}
+
+	public function render_shortcode( $atts = array(), $content = null ) {
+		$assets = new PPT_Assets();
+		$assets->enqueue_frontend_assets();
+
+		ob_start();
+		include ppt_get_asset_path( 'public/partials/app-root.php' );
+		return ob_get_clean();
+	}
+}
