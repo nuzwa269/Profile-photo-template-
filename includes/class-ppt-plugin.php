18e90c42c3b99369c3d6489f968bc3fa07c6a586
nuzwa269 <blsoha424@gmail.com>
2026-03-20 20:07:34 +0500

    Add PPT_Plugin class with initialization method

--- includes/class-ppt-plugin.php
+++ includes/class-ppt-plugin.php
@@ -1,1 +1,26 @@
+<?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class PPT_Plugin {
+
+	/**
+	 * @var PPT_Assets
+	 */
+	protected $assets;
+
+	/**
+	 * @var PPT_Shortcode
+	 */
+	protected $shortcode;
+
+	public function init() {
+		$this->assets    = new PPT_Assets();
+		$this->shortcode = new PPT_Shortcode();
+
+		$this->assets->init();
+		$this->shortcode->init();
+	}
+}
