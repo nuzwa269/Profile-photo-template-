89300f810e0f91688c61f1dd7f571e2a45b6cb08
nuzwa269 <blsoha424@gmail.com>
2026-03-21 15:45:22 +0500

    Add image file loading functionality to uploader

--- public/js/modules/uploader.js
+++ public/js/modules/uploader.js
@@ -1,1 +1,28 @@
+window.PPTUploader = (function () {
+	function loadImageFile(file, onSuccess) {
+		if (!file || !file.type.startsWith('image/')) {
+			return;
+		}
 
+		const reader = new FileReader();
+
+		reader.onload = function (event) {
+			const image = new Image();
+
+			image.onload = function () {
+				onSuccess({
+					image,
+					imageName: file.name
+				});
+			};
+
+			image.src = event.target.result;
+		};
+
+		reader.readAsDataURL(file);
+	}
+
+	return {
+		loadImageFile
+	};
+})();
