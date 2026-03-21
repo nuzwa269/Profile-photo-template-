d56d89e046b2b19cedb991bf6cd0eadb36e50e10
nuzwa269 <blsoha424@gmail.com>
2026-03-21 15:49:05 +0500

    Register and enqueue multiple PPT scripts

--- includes/class-ppt-assets.php
+++ includes/class-ppt-assets.php
@@ -19,9 +19,33 @@ class PPT_Assets {
 		);
 
 		wp_register_script(
+			'ppt-state-script',
+			ppt_get_asset_url( 'public/js/modules/state.js' ),
+			array(),
+			PPT_VERSION,
+			true
+		);
+
+		wp_register_script(
+			'ppt-renderer-script',
+			ppt_get_asset_url( 'public/js/modules/renderer.js' ),
+			array( 'ppt-state-script' ),
+			PPT_VERSION,
+			true
+		);
+
+		wp_register_script(
+			'ppt-uploader-script',
+			ppt_get_asset_url( 'public/js/modules/uploader.js' ),
+			array(),
+			PPT_VERSION,
+			true
+		);
+
+		wp_register_script(
 			'ppt-app-script',
 			ppt_get_asset_url( 'public/js/profile-tool-app.js' ),
-			array(),
+			array( 'ppt-state-script', 'ppt-renderer-script', 'ppt-uploader-script' ),
 			PPT_VERSION,
 			true
 		);
@@ -39,6 +63,9 @@ class PPT_Assets {
 
 	public function enqueue_frontend_assets() {
 		wp_enqueue_style( 'ppt-app-style' );
+		wp_enqueue_script( 'ppt-state-script' );
+		wp_enqueue_script( 'ppt-renderer-script' );
+		wp_enqueue_script( 'ppt-uploader-script' );
 		wp_enqueue_script( 'ppt-app-script' );
 	}
 }
